3565319ebc0570af9eee5540145fae81372047de
h4per <h4peryt@gmail.com>
2023-08-23 02:13:48 +0600

    7 commit

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from apps.courses.models import Courses, Benefits
 
 @admin.register(Courses)
-class CoureseAdmin(admin.ModelAdmin):
+class CoursesAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image', 'status']
     list_filter = ["status"]
     list_editable = ["status"]
 
 
 @admin.register(Benefits)
-class Benefits(admin.ModelAdmin):
+class BenefitsAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image', 'status']
     list_filter = ["status"]
     list_editable = ["status"]

f45049ed405cac18d73b066a5baf37d063670f38
h4per <h4peryt@gmail.com>
2023-08-23 02:27:01 +0600

    8 commit

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -4,12 +4,12 @@ from apps.courses.models import Courses, Benefits
 @admin.register(Courses)
 class CoursesAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image', 'status']
-    list_filter = ["status"]
-    list_editable = ["status"]
+    list_filter = ['status']
+    list_editable = ['status']
 
 
 @admin.register(Benefits)
 class BenefitsAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'image', 'status']
-    list_filter = ["status"]
-    list_editable = ["status"]
+    list_filter = ['status']
+    list_editable = ['status']

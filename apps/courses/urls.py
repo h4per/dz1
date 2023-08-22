c430c457470bf18360eb0f0be5334b986be0feec
h4per <h4peryt@gmail.com>
2023-08-23 01:01:12 +0600

    3commit

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.courses.views import CoursesView, BenefitsView
+from apps.courses.views import CoursesView, BenefitsView, MainMenuView
 
 
 urlpatterns = [
-    path('', CoursesView.as_view(), name='courses'),
-    path('benefits/', BenefitsView.as_view(), name='benefits'),
+    path('', MainMenuView.as_view(), name='main-menu'),
+    path('courses', CoursesView.as_view(), name='courses'),
+    path('benefits', BenefitsView.as_view(), name='benefits'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -5,6 +5,12 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 
+class MainMenuView(generic.ListView):
+    queryset = Courses.objects.all()
+    template_name = "courses/menu.html"
+    context_object_name = "courses"
+
+
 class CoursesView(generic.ListView):
     queryset = Courses.objects.all()
     template_name = "courses/course.html"

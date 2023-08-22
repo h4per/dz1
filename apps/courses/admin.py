50d0d3f61c002dcd4a73785903979bb3e46f0d0f
h4per <h4peryt@gmail.com>
2023-08-23 01:05:14 +0600

    4 commit

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from apps.courses.models import Courses
+from apps.courses.models import Courses, Benefits
 
 @admin.register(Courses)
 class CoureseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'image', 'status']
+    list_filter = ["status"]
+    list_editable = ["status"]
+
+
+@admin.register(Benefits)
+class Benefits(admin.ModelAdmin):
     list_display = ['title', 'description', 'image', 'status']
     list_filter = ["status"]
     list_editable = ["status"]
--- apps/courses/models.py
+++ apps/courses/models.py
@@ -24,3 +24,29 @@ class Courses(models.Model):
 class Meta:
     verbose_name = 'Курс'
     verbose_name_plural = 'Курсы'
+    
+
+class Benefits(models.Model):
+    title = models.CharField(
+        max_length= 255,
+        verbose_name='Заголовок'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    image = models.ImageField(
+        upload_to='courses/',
+        verbose_name='Фото',
+        blank=True
+    )
+    status = models.BooleanField(
+        default=True,
+        verbose_name="Статус публикации"
+    )
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
+
+class Meta:
+    verbose_name = 'Преимущество'
+    verbose_name_plural = 'Преимущества'
